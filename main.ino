1e6a6e0f3748a8a30eaa03ff972365c782c012e2
Zartarius <zaryabayaz2@gmail.com>
2025-07-07 19:59:09 +1000

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,1 +1,18 @@
 
+
+void setup(void) {
+  
+  
+}
+
+void loop(void) {
+  /*
+  while (front sensor detects object == true) {
+      move forward
+  }
+
+  while (front sensor detects object == false) {
+      sweeping motion
+  }
+  */
+}
