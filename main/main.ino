ae961b17059f59b69ca27edbcfb09e578c3fd001
Zartarius <zaryabayaz2@gmail.com>
2025-07-16 16:00:02 +1000

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -102,7 +102,7 @@ class USSensor {
     int trig_pin_;
     int echo_pin_;
   public:
-    USSensor() = default;
+    USSensor(void) = default;
     USSensor(int trig_pin, int echo_pin) : trig_pin_{trig_pin}, echo_pin_{echo_pin} {
       pinMode(trig_pin_, OUTPUT);
       pinMode(echo_pin_, INPUT);
@@ -125,19 +125,43 @@ class USSensor {
       float time = pulseIn(echo_pin_, HIGH);
       // distance formula
       return (time * 0.0343) / 2;
+      // same as return time * 0.01715
     }
 };
 
 Motor motor;
-USSensor sensor;
+USSensor us_sensor;
+
+static const float MAX_DIST = 100.0;
+static const float FORWARD_SPEED = 69.0; // to change
+static const float REVRSE_SPEED = 69.0; // to change
+static const float PIVOT_TIME = 1.0; // to change
 
 void setup(void) {
   Serial.begin(9600);
-  sensor = USSensor(6,5);
+  us_sensor = USSensor(6,5);
 }
 
 
 
 void loop(void) {
-  Serial.println(sensor.get_distance());
+  // Serial.println(sensor.get_distance());
+   {
+      float distance;
+      while ((distance = sensor.get_distance()) <= MAX_DIST /*&& IRsensor doesn't detect tape*/) {
+         motor.drive_forward(FORWARD_SPEED - distance); // Increase speed as it gets closer?
+      }
+   }
+   
+   motor.brake();
+   
+   // if (IR sensor detects tape) {
+   //    motor.drive_reverse(REVERSE_SPEED);
+   // }
+
+   for (float time = PIVOT_TIME; time < 1000000; time += PIVOT_TIME) {
+      // motor.pivot_right for time
+      // motor.pivot_left for 2 * time
+      // motor.pivot_right for time
+   }
 }
