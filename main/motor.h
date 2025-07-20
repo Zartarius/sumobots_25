215ca64100605cd17f265b49e5d9c7054384e6f7
Zartarius <zaryabayaz2@gmail.com>
2025-07-20 21:06:20 +1000

    Add do_for function

--- main/motor.h
+++ main/motor.h
@@ -3,16 +3,17 @@
 
 
 namespace Motor {
-bool do_for(const int movement, const int speed, 
-            const unsigned long ms_delay) {
-  const unsigned long start = millis();
+bool do_for(const int movement, const int speed, const unsigned long ms_delay) {
   if (movement == 0) {
       motor.pivot_left();
   } else if (movement == 1) {   
       motor.pivot_right();
   }
+            
+  const unsigned long start = millis();
   float distance;
   while ((distance = sensor.get_distance()) > MAX_DIST && (millis() - start) < ms_delay) {}
+            
   return distance <= MAX_DIST;
 }
 
